d6cbad9acd3e546bca3be875d58df05e2d9395af
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-16 14:19:50 +0000

    02_dataTypes updated

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+let Name="Vasu";
+let age="21";
+let email="vasu22300@gmail.com";
+let password="1234";
+let newstate;
+console.log(Name);
+console.table([Name,age,email,password,newstate]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  //Treat all JS code as newr version of js
+// alert(3+3);  //We are using Nodejs, not browser
+console.log("Vasu");
+console.log("I am good");
+
+
+
+/*
+1)Primitive Datatypes
+number =>2^53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined=>No value has been assigned
+symbol=>unique
+
+2)Objects
+*/
+
+//null is an object type
+console.log(typeof null);
+
+//undefined is a data type in itself
+console.log(typeof undefined);
