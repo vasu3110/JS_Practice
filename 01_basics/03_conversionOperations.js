f751294ea85facebb0cb3a7f1589e72c0a773d31
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-16 17:37:58 +0000

    03_conversionOperations.js file added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,47 @@
+let score="33";
+// console.log(typeof score);
+
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);
+
+let score1=true;
+// console.log(typeof score);
+
+let valueInNumber1=Number(score1);
+// console.log(typeof valueInNumber1);
+
+// console.log(valueInNumber1);
+
+/*
+"33"=>33
+"33abc"=>NaN
+true=>1
+false=>0
+*/
+
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+/*
+1=>true
+0=>false
+""=>false
+"Vasu"=>true
+*/
+
+let num=33;
+let strnum=String(num);
+console.log(strnum);
+console.log(typeof strnum);
+
+
+
+
+
+
+
+
+
+
+
