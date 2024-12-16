143e4350b34e5863b6c6a5861d87bc26384bec19
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-16 17:58:17 +0000

    Conversion Operations File updated

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,7 +21,7 @@ false=>0
 
 let isLoggedIn="";
 let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 1=>true
@@ -32,8 +32,37 @@ console.log(booleanIsLoggedIn);
 
 let num=33;
 let strnum=String(num);
-console.log(strnum);
-console.log(typeof strnum);
+// console.log(strnum);
+// console.log(typeof strnum);
+
+// **************Operations****************
+
+let value=3;
+let negValue=-value;
+
+// console.log(negValue);
+
+let str1="hello";
+let str2=" Vasu";
+let str3=str1+str2;
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gamecounter=1;
+console.log(++gamecounter);
+console.log(gamecounter);
+
+
+
+
 
 
 
