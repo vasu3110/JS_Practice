50a41487318d8f49b49d880a161b7aa47f662d39
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-16 18:26:13 +0000

    04_comparisons.js file added

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,25 @@
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+ //In case of > and >= which are comparators null is converted to a 0 as number whereas == is equality check and behaves differently
+
+//  console.log(undefined>0);
+//  console.log(undefined==0);
+//  console.log(undefined<0);
+
+ //undefined gives false in all the cases
+
+//Strict equality
+console.log("2"===2);
+
+
+
+ 
+ 
+ 
+
+
