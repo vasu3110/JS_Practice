083618f0ad1715db8f4523cd9fb0ae1957bf4af1
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-17 21:10:31 +0000

    05_strings added

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+let name="Vasu";
+const repocount=5;
+// console.log(`Hello My name is ${name} and my repo count is ${repocount}`);
+
+let gameName=new String("Vasu31__");
+console.log(gameName[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
