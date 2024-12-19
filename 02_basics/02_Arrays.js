e10a3b0d524678dd6aab086eaf480f00ef63d8d9
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-19 10:10:46 +0000

    02_Arrays completed

--- 02_basics/02_Arrays.js
+++ 02_basics/02_Arrays.js
@@ -22,3 +22,18 @@ console.log(all_new_heroes);
 const arr5=[1,2,3,[1,2,[4,5,[7,8]]]];
 const flatarr5=arr5.flat(Infinity);
 console.log(flatarr5);
+
+console.log(Array.isArray("Vasu"));
+console.log(Array.from("Vasu"));
+console.log(Array.from({name:"Vasu"})); //Interesting
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3));
+
+
+
+
+
