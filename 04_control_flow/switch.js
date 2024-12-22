1e772335d7acee55d4230762ec1cf3521e5b2a6e
Vasu Sharma <124486482+vasu3110@users.noreply.github.com>
2024-12-22 11:28:48 +0000

    switch case added

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//shift+alt+down to copy and paste a code
+const month=15;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Not a Valid Month");
+        break;
+}
+
+//If we forget a break in one of the cases then all the cases below it except the default are executed
